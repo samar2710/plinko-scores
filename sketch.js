eeef78e3e803fab71fb06f7b6180e77333b3603c
samar2710 <72249316+samar2710@users.noreply.github.com>
2021-10-04 17:01:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,104 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var balls = [];
+var ball;
+
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+  text("Score : "+score,20,30);
+  Engine.update(engine);
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-400, width/2+400), 10,10));
+     
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+if(ball!=null){
+  ball.display();
+
+   if(ball.body.position.y>500){
+   if(ball.body.position.x<300){
+     score = score + 500;
+     //ball = null;
+
+
+   }
+   else if(ball.body.position.x > 300 && ball.body.position < 600){
+     score = score + 100;
+     //ball=null;
+  }
+  else if(ball.body.positon.x > 600){
+    score = score+ 200; 
+    //ball= null;
+  }
+}
+}}
+function mousePressed(){
+ball = new Ball(mouseX,10,10);
+}
